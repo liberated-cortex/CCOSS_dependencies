70ff24b6b0c924dd57a895008223c7996b4409b2
Ian T. Jacobsen <iantj92@gmail.com>
2019-07-18 09:01:32 +0200

    Set the SDL prefix for when building SDL mixer

--- SDL_mixer.sh
+++ SDL_mixer.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+SDL_PREFIX=$PWD/SDL2-2.0.9
 pushd $PWD/SDL2_mixer-2.0.4
-    ./configure --prefix=$PWD/install --enable-shared=no --disable-sdltest --enable-music-opus=no --enable-music-mp3=no --enable-music-flac=no
+    ./configure --prefix=$PWD/install --enable-shared=no --disable-sdltest --enable-music-opus=no --enable-music-mp3=no --enable-music-flac=no --with-sdl-prefix=$SDL_PREFIX/install
     make
     make install
 popd
