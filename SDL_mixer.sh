ce21a035a6656f36dba3fc1249e7d7bdde7a428f
Ian T. Jacobsen <iantj92@gmail.com>
2019-07-18 08:23:22 +0200

    made the SDL mixer build script more precise

--- SDL_mixer.sh
+++ SDL_mixer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 pushd $PWD/SDL2_mixer-2.0.4
-    ./configure --prefix=$PWD/install --enable-shared=no
+    ./configure --prefix=$PWD/install --enable-shared=no --disable-sdltest --enable-music-opus=no --enable-music-mp3=no --enable-music-flac=no
     make
     make install
 popd
